f7f5c9feeb67658c22dbd0c47f99bf5caa110915
Ben <ben@aixmarketing4-BIS.local>
2022-04-21 16:44:22 +0200

    refactoring style badge

--- Layouts/home/index.php
+++ Layouts/home/index.php
@@ -101,7 +101,7 @@
                                  <?php foreach ($lists as $list) : ?>
 
                                  <tr>
-                                     <td><span class="badge badge-task m-2"><i class="lni lni-write"></i></span><?php echo $list['name'] ?? "" ?></td>
+                                     <td><span class="badge-task m-2"><i class="lni lni-write"></i></span><?php echo $list['name'] ?? "" ?></td>
 
 
 
