2247c3daec7e7af49c010abcd7a8737665539367
Ben <ben@aixmarketing4-BIS.local>
2022-04-22 15:26:09 +0200

    barre progress

--- Layouts/home/index.php
+++ Layouts/home/index.php
@@ -36,27 +36,27 @@
      </div>
      <!-- row -->
      <div class="container">
-     <div class="row">
+         <div class="row">
 
-         <div class="col-12">
-             <div class="card">
-                 <div class="card-header">
-                     <h1>Tasks manager</h4>
-                 </div>
-                 <div class="card-body">
-                     <div class="table-responsive">
-                         <table id="example4" class="display">
-                             <thead>
-                                 <tr>
-                                     <th>Vos listes</th>
+             <div class="col-12">
+                 <div class="card">
+                     <div class="card-header">
+                         <h1>Tasks manager</h4>
+                     </div>
+                     <div class="card-body">
+                         <div class="table-responsive">
+                             <table id="example4" class="display">
+                                 <thead>
+                                     <tr>
+                                         <th>Vos listes</th>
 
-                                     <th>Action</th>
-                                 </tr>
-                             </thead>
-                             <tbody>
-                                 <h2 class="col-12 text-center">Progression globale</h2>
-                                 <div class="progression row">
-                                     <?php
+                                         <th>Action</th>
+                                     </tr>
+                                 </thead>
+                                 <tbody>
+                                     <h2 class="col-12 text-center">Progression globale</h2>
+                                     <div class="progression row">
+                                         <?php
                                        
 
                                         $count_done = 0;
@@ -92,90 +92,97 @@
                                             
                                         $progression = round(($count_in_progress / count($tasks)) * 100);
                                         }
-                                       
+                                       ?>
+                                         <div class="progress">
+                                             <div class="progress-bar" role="progressbar" style="width: <?php echo  $progression ?>%;"
+                                                 aria-valuenow="<?php echo  $progression; ?>" aria-valuemin="0" aria-valuemax="100"><?php echo  $progression ?>%</div>
+                                         </div>
 
                                         
-                                        echo '<div class="col-12 col-lg-6 text-center">';
-                                        echo '<h4> Tâches en cours : <span class="badge badge-danger m-2">' . $progression . '%</span></h4>';
-                                        echo '</div>';
 
-                                        ?>
-                                 </div>
-                                 <?php foreach ($lists as $list) : ?>
+                                        
+                                     </div>
+                                     <?php foreach ($lists as $list) : ?>
 
-                                 <tr>
-                                     <td><span class="badge bg-info text-white m-2"><i class="lni lni-write"></i></span><?php echo $list['name'] ?? "" ?></td>
-
+                                     <tr>
+                                         <td><span class="badge bg-info text-white m-2"><i
+                                                     class="lni lni-write"></i></span><?php echo $list['name'] ?? "" ?>
+                                         </td>
 
 
 
-                                     <td>
-                                         <div class="widget-content-right ml-auto d-flex">
-                                             <form action="show-list/<?php echo $list['id'] ?? "" ?>" method="post">
-                                                 <input type="hidden" name="id" value="<?php echo $list['id'] ?? "" ?>">
-                                                 <button type="submit"
-                                                     class="border-0 btn-transition btn btn-outline-success">
-                                                     <i class="fa fa-eye"></i>
+
+                                         <td>
+                                             <div class="widget-content-right ml-auto d-flex">
+                                                 <form action="show-list/<?php echo $list['id'] ?? "" ?>" method="post">
+                                                     <input type="hidden" name="id"
+                                                         value="<?php echo $list['id'] ?? "" ?>">
+                                                     <button type="submit"
+                                                         class="border-0 btn-transition btn btn-outline-success">
+                                                         <i class="fa fa-eye"></i>
+                                                     </button>
+                                                 </form>
+                                                 <!--- Show formulaire edit-list-form -->
+                                                 <button type="button"
+                                                     class="edit-list border-0 btn-transition btn btn-outline-primary"
+                                                     data-toggle="modal"
+                                                     data-target="#editListModal<?php echo $list['id'] ?? "" ?>">
+                                                     <i class="fa fa-edit"></i>
                                                  </button>
-                                             </form>
-                                             <!--- Show formulaire edit-list-form -->
-                                             <button type="button"
-                                                 class="edit-list border-0 btn-transition btn btn-outline-primary"
-                                                 data-toggle="modal"
-                                                 data-target="#editListModal<?php echo $list['id'] ?? "" ?>">
-                                                 <i class="fa fa-edit"></i>
-                                             </button>
 
 
-                                             <form action="delete-list/<?php echo $list['id'] ?? "" ?>" method="post">
-                                                 <input type="hidden" name="id" value="<?php echo $list['id'] ?? "" ?>">
-                                                 <button type="submit"
-                                                     class="border-0 btn-transition btn btn-outline-danger" onclick="return confirm('Êtes-vous sûr de votre choix ?')">
-                                                     <i class="fa fa-trash"></i>
-                                                 </button>
-                                             </form>
-                                         </div>
-                                     </td>                                   
-                                 </tr>
-                                 <!--Modal edit list-->
-                                 <div class="modal fade" id="editListModal<?php echo $list['id'] ?? "" ?>"
-                                             tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-                                             aria-hidden="true">
-                                             <div class="modal-dialog" role="document">
-                                                 <div class="modal-content">
-                                                     <div class="modal-header">
-                                                         <h5 class="modal-title" id="exampleModalLabel">Edit List</h5>
-                                                         
-                                                         <button type="button" class="close" data-dismiss="modal"
-                                                             aria-label="Close">
-                                                             <span aria-hidden="true">&times;</span>
-                                                         </button>
-                                                     </div>
-                                                     <div class="modal-body">
+                                                 <form action="delete-list/<?php echo $list['id'] ?? "" ?>"
+                                                     method="post">
+                                                     <input type="hidden" name="id"
+                                                         value="<?php echo $list['id'] ?? "" ?>">
+                                                     <button type="submit"
+                                                         class="border-0 btn-transition btn btn-outline-danger"
+                                                         onclick="return confirm('Êtes-vous sûr de votre choix ?')">
+                                                         <i class="fa fa-trash"></i>
+                                                     </button>
+                                                 </form>
+                                             </div>
+                                         </td>
+                                     </tr>
+                                     <!--Modal edit list-->
+                                     <div class="modal fade" id="editListModal<?php echo $list['id'] ?? "" ?>"
+                                         tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+                                         aria-hidden="true">
+                                         <div class="modal-dialog" role="document">
+                                             <div class="modal-content">
+                                                 <div class="modal-header">
+                                                     <h5 class="modal-title" id="exampleModalLabel">Edit List</h5>
+
+                                                     <button type="button" class="close" data-dismiss="modal"
+                                                         aria-label="Close">
+                                                         <span aria-hidden="true">&times;</span>
+                                                     </button>
+                                                 </div>
+                                                 <div class="modal-body">
                                                      <form class="edit-list-form"
-                                                             action="edit-list/<?php echo $list['id'] ?? "" ?>"
-                                                             method="post">
-                                                             <input type="hidden" name="id"
-                                                                 value="<?php echo $list['id'] ?? "" ?>">
-                                                             <input type="text" name="name"
-                                                                 value="<?php echo $list['name'] ?? "" ?>">
-                                                             <button type="submit"
-                                                                 class="border-0 btn-transition btn btn-outline-primary">
-                                                                 envoyer
-                                                             </button>                                                         
+                                                         action="edit-list/<?php echo $list['id'] ?? "" ?>"
+                                                         method="post">
+                                                         <input type="hidden" name="id"
+                                                             value="<?php echo $list['id'] ?? "" ?>">
+                                                         <input type="text" name="name"
+                                                             value="<?php echo $list['name'] ?? "" ?>">
+                                                         <button type="submit"
+                                                             class="border-0 btn-transition btn btn-outline-primary">
+                                                             envoyer
+                                                         </button>
 
-                                                         </form>
-                                                     </div>
+                                                     </form>
                                                  </div>
                                              </div>
                                          </div>
-                                 <?php endforeach; ?>
+                                     </div>
+                                     <?php endforeach; ?>
 
-                             </tbody>
-                         </table>
+                                 </tbody>
+                             </table>
+                         </div>
                      </div>
                  </div>
              </div>
          </div>
-    </div>
-</div>
+     </div>
